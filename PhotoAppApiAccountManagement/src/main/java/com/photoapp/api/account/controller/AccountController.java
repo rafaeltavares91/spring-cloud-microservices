c274892741c4cf581a7cd9c4dbf5e62a6b85916b
rafael.tavares <rafaeltavares91@gmail.com>
2019-05-29 21:34:55 -0300

    creating account management microservice

--- PhotoAppApiAccountManagement/src/main/java/com/photoapp/api/account/controller/AccountController.java
+++ PhotoAppApiAccountManagement/src/main/java/com/photoapp/api/account/controller/AccountController.java
@@ -1,12 +1,12 @@
-package com.photoapp.api.users.controller;
+package com.photoapp.api.account.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/users")
-public class UserController {
+@RequestMapping("/account")
+public class AccountController {
 
 	@GetMapping("/status/check")
 	public String status() {

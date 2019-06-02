6d16219caddefbf79b308e374308e99effac5a1b
rafael.tavares <rafaeltavares91@gmail.com>
2019-06-01 23:48:50 -0300

    creating eureka instance-id property

--- PhotoAppApiUsers/src/main/java/com/photoapp/api/users/controller/UserController.java
+++ PhotoAppApiUsers/src/main/java/com/photoapp/api/users/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.photoapp.api.users.controller;
 
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
+	private Environment env; 
+	
+	public UserController(Environment env) {
+		this.env = env;
+	}
+	
 	@GetMapping("/status/check")
 	public String status() {
-		return "working";
+		return "working on port "+ env.getProperty("local.server.port");
 	}
 	
 }

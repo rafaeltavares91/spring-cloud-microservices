f3bc1a906e2f3aa856710fe20f9585376f5ee73e
rafael.tavares <rafaeltavares91@gmail.com>
2019-06-02 22:27:07 -0300

    allow only IP address of Zuul API Gateway

--- PhotoAppApiUsers/src/main/java/com/photoapp/api/users/security/WebSecurity.java
+++ PhotoAppApiUsers/src/main/java/com/photoapp/api/users/security/WebSecurity.java
@@ -1,6 +1,7 @@
 package com.photoapp.api.users.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -9,10 +10,18 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
 
+	private Environment environment;
+	
+	public WebSecurity(Environment environment) {
+		this.environment = environment;
+	}
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable();
-		http.authorizeRequests().antMatchers("/users/**").permitAll();
+		http.authorizeRequests()
+			.antMatchers("/**")
+			.hasIpAddress(environment.getProperty("gateway.ip"));
 	}
 	
 }
